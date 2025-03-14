31e2cc86120238488866b7b42d7fd53edc4a7a7f
Maththehecker <153492417+Maththehecker@users.noreply.github.com>
2025-03-14 10:30:34 +0100

    panel pause + proto keybinds tuto

--- Assets/AbilityManager.cs
+++ Assets/AbilityManager.cs
@@ -9,22 +9,22 @@ public class AbilityManager : MonoBehaviour
 
     void Start()
     {
-        // Trouve toutes les capacités attachées au même GameObject
+
         Abilities.AddRange(GetComponents<Ability>());
         if (Abilities.Count > 0)
         {
-            SetActiveAbility(0); // Sélectionne la 1ere capacité au début
+            SetActiveAbility(0); 
         }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q)) // Changer de capacite
+        if (Input.GetKeyDown(KeyCode.Q)) 
         {
             CycleAbility();
         }
 
-        if (Input.GetKeyDown((KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("interact", "F")))) // Activer la capacite numero 1
+        if (Input.GetKeyDown((KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("interact", "F")))) 
         {
             Debug.Log(Abilities[currentAbilityIndex].AbilityName);
             activeAbility?.Activate();
