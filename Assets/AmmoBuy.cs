fdd81d181b7f73a7ec0da79ecc8bee58a3ee33cb
Maththehecker <153492417+Maththehecker@users.noreply.github.com>
2025-04-12 09:03:59 +0200

    interface shop moderne

---
+++ Assets/AmmoBuy.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AmmoBuy : MonoBehaviour
+{
+    public GameObject player;
+
+    public GameObject Money;
+    
+
+    //je dois attendre que la monaie du jeu et que le niveau de munitions soit implémenté
+    public void Buy100(){
+        
+    }
+
+    public void Buy250(){
+
+    }
+
+    public void Buy500(){
+        
+    }
+
+}
---
+++ Assets/WeaponUpgrade.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class WeaponUpgrade : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Money;
+    // je dois attendre que l'on implémente la monnaie et les différentes amméillorations
+    public void Upgrade(){
+
+    }
+}
