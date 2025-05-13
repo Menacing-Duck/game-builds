153457c1d5b778d5856308ad2db85061b8f95b22
Maththehecker <153492417+Maththehecker@users.noreply.github.com>
2025-05-13 09:27:23 +0200

    esquisses pathfinding

---
+++ Assets/Scripts/MainPathfinding.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MainPathfinding : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
