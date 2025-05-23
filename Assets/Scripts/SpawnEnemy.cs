aeddca7c19911f62e41878d71ae4c9212af91823
Maththehecker <153492417+Maththehecker@users.noreply.github.com>
2025-05-23 23:46:13 +0200

    nettoyage scripts

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -10,16 +10,4 @@ public class SpawnEnemy : MonoBehaviour
     {
         Instantiate(Monster, transform.position, transform.rotation);
     }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
